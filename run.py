ac478fb8156a45c4f776897e41eae6f21f6f5226
dungxibo123 <votiendung0805@gmail.com>
2021-12-09 16:12:34 +0700

    change run.py

--- run.py
+++ run.py
@@ -164,9 +164,11 @@ elif DATA_TYPE=="svhn":
 ds_len_, ds_ = preprocess_data(DATA,data_type=DATA_TYPE,device=device, sigma=None)
 _, perturbed_ds_ = preprocess_data(DATA,data_type=DATA_TYPE, device=device, sigma=[15.])
 #sigmas = [0.0,1e-5,10.0, 15.0,20.0]
-#loaders = [(key,DataLoader(preprocess_data(SVHN, sigma=[key,key], device=device, train=True)[1], batch_size=12000)) for key in sigma]
-sde_model = main( ds_len_, ds_, perturbed_ds_, device=device, model_type="sde", data_name=f"svhn_origin", batch_size=BATCH_SIZE, 
-    epochs=EPOCHS, train_num=TRAIN_NUM, valid_num=VALID_NUM, test_num=TEST_NUM, result_dir=RESULT_DIR, parallel=PARALLEL, integral_type=INTEGRAL_TYPE, solver=SOLVER, noise_type=NOISE_TYPE, weight_decay=WEIGHT_DECAY)
+#loaders = [(key,DataLoader(preprocess_data(SVHN, sigma=[key,key], device=device, train=True)[1], batch_size=12000)) for key in sigma
+sde_model = main( ds_len_, ds_, perturbed_ds_, device=device, model_type="sde", data_name=f"svhn_origin", batch_size=BATCH_SIZE,
+                    epochs=EPOCHS, train_num=TRAIN_NUM, valid_num=VALID_NUM, test_num=TEST_NUM, 
+                    result_dir=RESULT_DIR, parallel=PARALLEL, integral_type=INTEGRAL_TYPE, solver=SOLVER,
+                    noise_type=NOISE_TYPE, weight_decay=WEIGHT_DECAY)
 """
 if isinstance(sde_model, nn.DataParallel): sde_model = sde_model.module
 for k,l in loaders:
